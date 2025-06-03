657680a7379a1627610c950cfbdc14146887b772
Ho Mei-Yi <mei-yi.ho9@etu.univ-lorraine.fr>
2025-06-03 15:04:58 +0200

    GREEN

--- MineSweeper.test.ts
+++ MineSweeper.test.ts
@@ -1,5 +1,16 @@
 import { assertEquals } from "jsr:@std/assert";
 
+function rechecheMineCote(i:number):number{
+    const premirerLigne=[".",".","*","*","."];
+    let compteurMine=0;
+
+    if(premirerLigne[i+1]==="."){
+        compteurMine=compteurMine;
+    }
+
+    return compteurMine;
+}
+
 Deno.test("0 mines à droite", ()=>{
     assertEquals(rechecheMineCote(0),0);
 });
